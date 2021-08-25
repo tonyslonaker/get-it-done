42037a55e1b2e4b07c3bfd67ab311fbb28602c6d
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-25 17:51:02 -0600

    update getUserRepos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,24 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/tonyslonaker/repos");
+var getUserRepos = function(user) {
+  //format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  //make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+
+
+
+
+
+
+//var response = fetch("https://api.github.com/users/tonyslonaker/repos").then(function(response) {
+  //response.json().then(function(data) {
+    //console.log(data);
+  //});
+//});
+//console.log("outside");
